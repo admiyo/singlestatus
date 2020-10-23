8a4f70ad83113aebf92a3c2e0d63756b21b34728
Adam Young <ayoung@redhat.com>
2020-10-23 11:09:46 -0400

    borders are properly emboldened

--- sudoku.py
+++ sudoku.py
@@ -5,13 +5,47 @@ BASIS=3
 DIM=BASIS * BASIS
 HALFISH=(DIM-1)/2
 
+def is_bold(val):
+    return val % BASIS == 0
 
+def draw_tee(tee, row, col):
+    piece = ord(tee[0])
+    if is_bold(row):
+        piece = piece + 3
+    if is_bold(col):
+        piece = piece + 4
+    print ("%c"%piece, end='')
 
+cross_bars = ["┼","┿","╂","╋"]
+    
+def draw_cross(row, col):
+    piece = 0
+    if is_bold(row):
+        piece = piece + 1
+    if is_bold(col):
+        piece = piece + 2
+    print ("%c"%cross_bars[piece], end='')
 
+FULL_SET="123456789"
 
 def draw_set():
-    print("123456789", end='')
+    print(FULL_SET, end='')
 
+def draw_padding():
+    print("         ", end='')
+
+def pad_row():
+    for col in range (DIM):
+        if col % BASIS != 0  :
+            print("│", end="")
+        else:
+            print("┃", end="")
+        draw_padding()
+    print("┃")
+
+def remove_from_set(cell_set, val):
+    if (val < 0) | (val > 9):
+        assert("invalid value %s" %val)
 
 def draw_cell_length(bold=False):
     for _ in range(9):
@@ -19,42 +53,59 @@ def draw_cell_length(bold=False):
             print ("━", end='')
         else:
             print ("─", end='')
-
-    
-def draw_board():
-     print("┏", end='') 
-     for col in range (DIM - 1):
+            
+def draw_board(board):
+     print ("┏",end="")
+     for col in range (DIM):
+         if col > 0:
+             draw_tee('┬', 0, col)
          draw_cell_length(True)
-         if col % BASIS == BASIS -1:
-             print("┳", end="")
-         else:
-             print ("┯", end='')
-     draw_cell_length(True)
      print("┓") 
 
      for row in range (DIM):
+
+         if (row > 0):
+             if is_bold(row) :
+                 print("┣", end='')
+             else:
+                 print("┠", end='')
+                 
+             for col in range (DIM):
+                 if col > 0:
+                     draw_cross(row, col)
+                 draw_cell_length(is_bold(row))
+             if is_bold(row):                 
+                 print("┫", end='')
+             else:
+                 print("┨", end='')
+             print("") 
+
+         pad_row()
+                      
          for col in range (DIM):
              if col % BASIS != 0  :
                  print("│", end="")
              else:
                  print("┃", end="")
-             draw_set()
+             print(board[row][col], end="")
          print("┃")
+         pad_row()
 
-         if (row < 8):
-             print("┠", end='') 
-             for col in range (DIM-1):
-                 draw_cell_length()
-                 print ("┼", end='')
-             draw_cell_length()
-             print("┤") 
 
-     print("┗", end='') 
-     for col in range (DIM-1):
-         draw_cell_length(True)
-         print ("┷", end='')
-     draw_cell_length(True)
+     print("┗", end='')
+     for col in range (DIM):
+         if col > 0:
+             draw_tee('┴', 0, col)
+         draw_cell_length(True)     
      print("┛") 
 
-draw_board()
+board = []
+
+for row in range(DIM):
+    cur_row = []
+    for col in range (DIM):
+        cur_row.append(FULL_SET)
+    board.append(cur_row)
+     
+draw_board(board)
      
